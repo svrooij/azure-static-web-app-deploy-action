27a59cb9300034d74a2038d5f2867b4eb15f5179
Stephan van Rooij <1292510+svrooij@users.noreply.github.com>
2025-06-04 13:51:14 +0200

    Run a script to deploy the application

---
+++ deploy.ps1
@@ -0,0 +1,58 @@
+param (
+  
+  [Parameter(Mandatory = $true, HelpMessage = "Path to files to deploy")]
+  [string]$AppLocation,
+  [Parameter(Mandatory = $true, HelpMessage = "Static web app environment")]
+  [string]$Environment,
+
+  [Parameter(Mandatory = $false, HelpMessage = "Path to API files to deploy")]
+  [string]$ApiLocation = $null
+)
+
+BEGIN {
+  $ErrorActionPreference = "Stop"
+  $PSDefaultParameterValues["*:ErrorAction"] = "Stop"
+
+  if (-not (Get-Command swa -ErrorAction SilentlyContinue)) {
+    Write-Host "::error title=SWA CLI not found::Please ensure SWA CLI is installed."
+    exit 1
+  }
+
+  if (-not (Test-Path $AppLocation)) {
+    Write-Host "::error title=App Location not found::The specified app location '$AppLocation' does not exist."
+    exit 1
+  }
+
+  if ($ApiLocation -and -not (Test-Path $ApiLocation)) {
+    Write-Host "::error title=API Location not found::The specified API location '$ApiLocation' does not exist."
+    exit 1
+  }
+
+  # Build the SWA CLI command parameters
+  Write-Host "Starting deployment using SWA CLI..."
+  $cliParameters = @{
+    # app_location = $AppLocation
+    # output_location = $OutputLocation
+    env = $Environment
+  }
+
+  if ($ApiLocation) {
+    $cliParameters.api_location = $ApiLocation
+  }
+
+  # Convert parameters to a format suitable for the swa command
+  $swaCommand = "swa deploy $AppLocation"
+  foreach ($key in $cliParameters.Keys) {
+    $swaCommand += " --$key $($cliParameters[$key])"
+  }
+
+  Write-Host "Command: $swaCommand"
+  # Execute the SWA CLI command
+  try {
+    Invoke-Expression $swaCommand
+  } catch {
+    Write-Host "::error title=Deployment failed::An error occurred during deployment ❌: $_"
+    exit 1
+  }
+  Write-Host "Deployment completed successfully."
+}
